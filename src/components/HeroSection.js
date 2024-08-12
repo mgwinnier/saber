c17433e60cbd9c9b80468484e233f454014390a3
mgwinnier <mgwinnier@gmail.com>
2024-08-12 08:56:08 -0500

    Update HeroSection.js

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -12,9 +12,7 @@ function HeroSection() {
                     <h1 className="text-4xl md:text-5xl font-bold mb-4" style={{ textShadow: '2px 2px 4px rgba(0, 0, 0, 0.75)' }}>
                         Saber Construction, LLC
                     </h1>
-                    <p className="text-2xl md:text-3xl font-semibold" style={{ textShadow: '2px 2px 4px rgba(0, 0, 0, 0.75)' }}>
-                        Certified Service-Disabled Veteran’s Owned Small Business
-                    </p>
+                    
                 </div>
             </div>
         </section>
